83c1cf7ffb73c5ebcb103dde7bc725090f64b304
JS <frostmist118@gmail.com>
2025-08-08 12:51:27 +0900

    커밋

--- GitTest/src/ex01/ex01.java
+++ GitTest/src/ex01/ex01.java
@@ -1,9 +1,9 @@
-package ex01GitTest;
+package ex01;
 
 public class ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(" 나는 팀장이다");
 
 	}
 
